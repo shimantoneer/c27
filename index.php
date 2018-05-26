4d43200398a65eb2b6647c4fb6d99a351d6ce76d
shimantoneer <poddernakla@gmail.com>
2018-05-26 23:34:27 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.einsteintheplay.com/Einstein/Einstein_Home.html");
 die();
 ?>
